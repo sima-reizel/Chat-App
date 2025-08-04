110489e8b11523f69d6cbdee8ebe64002abd82b3
sima-reizel <127658952+sima-reizel@users.noreply.github.com>
2025-08-04 23:22:31 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,13 +10,13 @@ import {validAuth} from './middleware/authMiddleware.js'
 dotenv.config()
 
 const app = express()
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
-})
+});
 
 const PORT = process.env.PORT || 5000
 
@@ -24,16 +24,17 @@ app.use(cors())
 app.use(json())
 app.use(authRoutes)
 app.use(validAuth)
-app.use('/api/groups', groupRoutes)
+app.use('/api/groups', groupRoutes);
+
 
 io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id)
+  console.log('A user connected:', socket.id);
   socket.on('join-room', (groupName) => {
-    socket.join(groupName)
-    io.to(groupName).emit('user-joined', { userId: socket.id })
-  })
-})
+    socket.join(groupName);
+    io.to(groupName).emit('user-joined', { userId: socket.id });
+  });
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
